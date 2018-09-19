fe203af13c38057cf9b2cd17bd49c07a0b0533e5
chaoswang <chaoswang@163.com>
2018-09-19 23:44:53 +0800

    angular.cn-英雄指南-day1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +7,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
-  heroes = HEROES;
-  
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
