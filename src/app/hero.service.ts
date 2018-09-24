d5778a07a08dffe9fb9d99dfe5f48188a7a2702d
chaoswang <chaoswang@163.com>
2018-09-24 20:57:37 +0800

    angular.cn-英雄指南-day3

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,20 @@
 import {Injectable} from '@angular/core';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
+import {Observable} from 'rxjs/internal/Observable';
+import {of} from 'rxjs/internal/observable/of';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() {
+  constructor(private messageService: MessageService) {
   }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService:fetched heroes');
+    return of(HEROES);
   }
 }
